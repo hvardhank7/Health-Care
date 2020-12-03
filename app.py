c4ae8c9714ed525d7a3521679d5157429e489c22
Harshvardhan Kushwaha <49608034+hvardhank7@users.noreply.github.com>
2020-12-04 00:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app=Flask(__name__,template_folder='template')
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
-@app.route("/home.html")
+@app.route("/")
 def home():
     return render_template("home.html")
 
